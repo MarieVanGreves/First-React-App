7444c1358a712a5a392def09b0d5bfe01969ea75
MarieVanGreves <121773626+MarieVanGreves@users.noreply.github.com>
2023-04-12 14:34:38 +0200

    Adding missing files

---
+++ src/assets/PokemonCard.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+function PokemonCard(props) {
+  console.log(props.pokemon);
+  return (
+    <figure>
+      {props.pokemon.imgSrc ? <img src={props.pokemon.imgSrc} alt={props.pokemon.name} /> : <p>???</p>}
+      <figcaption> {props.pokemon.name} </figcaption>
+    </figure>
+  );
+}
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
+};
+
+export default PokemonCard;
